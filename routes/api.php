2d66c6317498d698fec63b719a55c170963aba6b
Jhonna <jhonna.somarriba@gmail.com>
2023-08-10 17:01:57 -0600

    Se agruparon los middlewares

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', 'App\Http\Controllers\Api\PostController')->middleware(['auth:sanctum']);
+//Agrupar los middlewares
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('posts', 'App\Http\Controllers\Api\PostController');
+    Route::apiResource('comments', 'App\Http\Controllers\Api\CommentController');
+    Route::apiResource('likes', 'App\Http\Controllers\Api\LikeController');
+    Route::apiResource('shares', 'App\Http\Controllers\Api\ShareController');
+    Route::apiResource('saved', 'App\Http\Controllers\Api\SavedController');
+});
