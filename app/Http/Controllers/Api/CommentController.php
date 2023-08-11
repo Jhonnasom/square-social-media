8294db937c6f0eeeadc0650b97daf5a14601f205
Jhonna <jhonna.somarriba@gmail.com>
2023-08-10 18:23:47 -0600

    Se mejora el CRUD del Api/CommentController

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Comment;
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,16 +12,16 @@ class CommentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-      $comments = Comment::orderBy('id', 'desc')->get();
+      $comments = $post->comments()->orderBy('id', 'desc')->get();
       return response()->json($comments);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Post $post, Request $request)
     {
       $request->validate([
         'description' => 'required',
@@ -28,6 +29,7 @@ class CommentController extends Controller
 
       return auth()->user()->comments()->create([
         'description' => $request->description,
+        'post_id' => $post->id,
       ]);
     }
 
@@ -42,13 +44,15 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comment)
+    public function update(Post $post, Request $request, Comment $comment)
     {
       $request->validate([
         'description' => 'required',
       ]);
 
-      $comment->update($request->all());
+      $comment->update([
+        'description' => $request->description,
+      ]);
 
       return response()->json('Comment updated!');
     }
@@ -56,7 +60,7 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Comment $comment)
+    public function destroy(Post $post, Comment $comment)
     {
       $comment_found= auth()->user()->comments()->findOrFail($comment->id);
       $comment_found->delete();
