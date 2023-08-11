2bfd6dfd1be90ea8dfaddfaea56d52fb358036d1
Jhonna <jhonna.somarriba@gmail.com>
2023-08-10 18:24:42 -0600

    Se mejora el CRUD del Api/Like/Controller

--- app/Http/Controllers/Api/LikeController.php
+++ app/Http/Controllers/Api/LikeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Like;
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,17 +12,26 @@ class LikeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-        //
+      $likes = $post->likes()->orderBy('id', 'desc')->get();
+      return response()->json($likes);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Post $post, Request $request)
     {
-        //
+        $like = auth()->user()->likes()->where('post_id', $post->id)->first();
+
+        if ($like == null) {
+          return $like = auth()->user()->likes()->create([
+            'post_id' => $post->id,
+          ]);
+        } else {
+          return response()->json('Like already exists!');
+        }
     }
 
     /**
@@ -43,8 +53,10 @@ class LikeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Like $like)
+    public function destroy(Post $post, Like $like)
     {
-        //
+        $like_found = auth()->user()->likes()->findOrfail($like->id);
+        $like_found->delete();
+        return response()->json('Like deleted!');
     }
 }
