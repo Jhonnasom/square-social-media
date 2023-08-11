04e3612b607c658eaa85de7e0a25136efea78ad2
Jhonna <jhonna.somarriba@gmail.com>
2023-08-10 18:25:55 -0600

    Se mejora el CRUD del Api/Saved/Controller

--- app/Http/Controllers/Api/SavedController.php
+++ app/Http/Controllers/Api/SavedController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Saved;
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,17 +12,20 @@ class SavedController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-        //
+      $saved = $post->saved()->orderBy('id', 'desc')->get();
+      return response()->json($saved);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Post $post, Request $request)
     {
-        //
+        return $saved = auth()->user()->saved()->create([
+          'post_id' => $request->post_id,
+        ]);
     }
 
     /**
@@ -45,6 +49,8 @@ class SavedController extends Controller
      */
     public function destroy(Saved $saved)
     {
-        //
+        $saved_found = auth()->user()->saved()->findOrfail($saved->id);
+        $saved_found->delete();
+        return response()->json('Unsaved!');
     }
 }
