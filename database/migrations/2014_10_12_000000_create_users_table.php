9450516ab23fc6ffb97865138c1e5b82653664f8
Jhonna <jhonna.somarriba@gmail.com>
2023-08-13 23:49:05 -0600

    Se corrige migracion de usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('alias')->nullable()->default(null);
             $table->string('description')->nullable()->default(null);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
